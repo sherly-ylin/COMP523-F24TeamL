b35b511ee7ffc3a981b67f9f7ff1a1f4a0fa9505
Hengyi(Henry) Liu <liuheng1@unc.edu>
2024-11-13 23:06:23 -0500

    Add set up review page

---
+++ my-app/src/app/components/set-up-review-page/set-up-review-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SetUpReviewPageComponent } from './set-up-review-page.component';
+
+describe('SetUpReviewPageComponent', () => {
+  let component: SetUpReviewPageComponent;
+  let fixture: ComponentFixture<SetUpReviewPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SetUpReviewPageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SetUpReviewPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
