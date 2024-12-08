7a2a4b2e0715911717a0cedbf0612b60688dce6c
Sherly Yixuan Lin <linyixuansherly@gmail.com>
2024-12-08 18:45:12 -0500

    Invite user change form

--- my-app/src/app/components/invite-user/invite-user.component.ts
+++ my-app/src/app/components/invite-user/invite-user.component.ts
@@ -1,13 +1,20 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { AuthService, Profile } from 'src/app/auth.service';
-import { FormsModule } from '@angular/forms';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Team, TeamService } from 'src/app/services/team.service';
 @Component({
   selector: 'app-invite-user',
@@ -15,32 +22,39 @@ import { Team, TeamService } from 'src/app/services/team.service';
   styleUrls: ['./invite-user.component.css'],
   imports: [
     FormsModule,
+    ReactiveFormsModule,
     MatRadioModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule,
     MatSelectModule,
+    CommonModule, NgFor
   ],
 })
-export class InviteUserComponent {
+export class InviteUserComponent implements OnInit {
   user_role: string;
-  email: string;
+  // email: string;
   role: string;
   teams: Team[];
-  timespanString: string;
-  timespan: { value: number; unit: string };
+  teamSelected: string;
+  // timespanString: string;
+  // timespan: { value: number; unit: string };
   submitted: boolean;
+  
+  inviteForm!: FormGroup;
 
   constructor(
     private router: Router,
     private http: HttpClient,
     private authService: AuthService,
-    private teamService: TeamService
+    private teamService: TeamService,
+    private formBuilder: FormBuilder
   ) {
-    this.email = '';
+    // this.email = '';
     this.role = 'provider';
-    this.timespanString = '3 day';
-    this.timespan = { value: 3, unit: 'day' };
+    this.teamSelected = '';
+    // this.timespanString = '3 day';
+    // this.timespan = { value: 3, unit: 'day' };
     this.submitted = false;
 
     this.user_role = this.authService.currentUser?.role ?? '';
@@ -62,23 +76,53 @@ export class InviteUserComponent {
     });
   }
 
-  onTimespanChange() {
-    const [value, unit] = this.timespanString.split(' '); // Split string into value and unit
-    this.timespan = {
+  ngOnInit(): void {
+    this.authService.getProfile().subscribe((profile: Profile | null) => {
+      if (profile) {
+        console.log('invite user: ', profile);
+        this.user_role = profile.role;
+        console.log('user_role: ', this.user_role);
+      }
+    });
+
+    this.loadTeams();
+
+    this.inviteForm = this.formBuilder.group({
+      email: ['', Validators.required, Validators.email],
+      role: [this.role, Validators.required],
+      selectedTeam: [null],
+      timespanString: ['3 day', Validators.required],
+    });
+  }
+
+  loadTeams(): void {
+    this.teamService.getTeams().subscribe((teams) => (this.teams = teams));
+  }
+  
+  getTimespan(timespanString: string) {
+    const [value, unit] = timespanString.split(' '); // Split string into value and unit
+    let timespan = {
       value: parseInt(value, 10), // Convert the decimal value part to a number
       unit: unit || 'day', // Default to "day" if no unit found
     };
+    return timespan;
   }
-
+  onRoleChange(){
+    const selectedTeamControl = this.inviteForm.get('selectedTeam');
+    if(this.inviteForm.value.role === 'provider'){
+      selectedTeamControl?.setValidators([Validators.required]);
+    }else {
+      selectedTeamControl?.clearValidators();
+    }
+  }
   public onSubmit() {
     this.submitted = true;
-
-    if (this.email && this.role) {
+    if (this.inviteForm.valid) {
       this.http
         .post('http://localhost:3000/api/auth/invite', {
-          email: this.email,
-          role: this.role,
-          timespan: this.timespan,
+          email: this.inviteForm.value.email,
+          role: this.inviteForm.value.role,
+          timespan: this.getTimespan(this.inviteForm.value.timespanString),
         })
         .subscribe({
           next: (response) => {
